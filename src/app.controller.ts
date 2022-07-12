662495ee7928310f219ae3506699176309604930
inmass <inmass.idbel@gmail.com>
2022-07-12 17:27:08 +0100

    Integration ✔️

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  
+  @Get('/air-quality')
+  getAirQuality(@Query('latitude') latitude: number, @Query('longitude') longitude: number) {
+    return this.appService.getAirQuality(latitude, longitude); // send parameters to service
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { map, Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly httpService: HttpService) {}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getAirQuality(latitude:number, longitude:number) {
+    // create API url with latitude and longitude parameters
+    let url = `https://api.airvisual.com/v2/nearest_city?lat=${latitude}&lon=${longitude}&key=6bb893a4-7b0c-4771-bc90-8042d8b77d1b`;
+    let response = this.httpService.get(url).pipe(
+      map(response => {
+        return {
+          Result: {
+            'Pollution': response.data.data.current.pollution
+          }
+        }
+      }
+    ));
+    return response;
+  }
+}
